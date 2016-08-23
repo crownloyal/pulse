fa047be4433af7820287990a4834e84b54b7203e
Brause, Dominic <Dominic.Brause@emc.com>
2016-08-23 14:12:42 +0100

    + router :id fixed + api namespace

--- app/routes/dashboard/board.js
+++ app/routes/dashboard/board.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(param){
-        return this.store.findRecord('board',param.post_id);
+        return this.store.findRecord('dashboard',param.id);
     }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -9,17 +9,8 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  //this.namespace = 'api';    // make this `api`, for example, if your API is namespaced
+  this.namespace = 'api';    // make this `api`, for example, if your API is namespaced
   this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id'); */
 
 
   this.get('/dashboards', function() {
@@ -29,7 +20,10 @@ export default function() {
       text: 'Yadda yadda yadda.'},
     { id: 2,
       name: 'Finance',
-      text: 'Yeah yeah yeah.'}]
+      text: 'Yeah yeah yeah. $$$'},
+    { id: 3,
+      name: 'Additional Dashboard',
+      text: 'Ohhhhh wow.'}]
     };
   });
 
@@ -49,7 +43,14 @@ export default function() {
     };
   });
 
-  /*
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
+ this.get('/profile', function() {
+    return { "profile": [{ 
+      id: 1,
+      name: 'Dominic',
+      lastname: 'Brause',
+      email: 'dominic.brause@emc.com',
+      image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAhCAAAAJGUwMTBlMTc4LWQxN2MtNDQ1YS1iZGQ4LTg3NzNhYTVhNzViMw.jpg'}]
+    };
+  });
+
 }
