09ec7a63178a945ea944011e369a27ce0c95fdd5
Brause, Dominic <Dominic.Brause@emc.com>
2016-08-30 17:43:59 +0100

    + actions in display-dashboard & fixed value in /dashboard

--- app/components/dashboard-display.js
+++ app/components/dashboard-display.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+	actions: {
+		toggleEditDashboard: fuction() {
+			this.toggleProperty('isEditingDashboard');
+		}
+	}
 });
