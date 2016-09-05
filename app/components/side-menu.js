38888852f1b65e1298ae6f179cf0885af7dff17d
AEROdom <dom.brause@gmail.com>
2016-09-05 19:54:21 +0100

    side-nav mobile support
    + added hammerjs
    + added swipe and tap support
    + smaller styling changes
    + depencies

--- app/components/side-menu.js
+++ app/components/side-menu.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
+import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
-export default Ember.Component.extend({
+const { Component } = Ember;
+
+export default Ember.Component.extend(RecognizerMixin, {
 
 	menuVisible: false,
 
 	toggleMenuVisibility(){
 		this.toggleProperty('menuVisible');
-	}
+	},
+ 	tapStart(e) {
+ 		toggleMenuVisibility();
+ 	},
+
+
+	recognizers: 'swipe tap'
 });
