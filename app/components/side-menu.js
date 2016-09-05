0a52872006956a345cd9960adc08749a4e5f6dd9
Brause, Dominic <Dominic.Brause@emc.com>
2016-09-05 16:10:39 +0100

    + added visibility toggle to side-nav + moved side-nav out of frame

--- app/components/side-menu.js
+++ app/components/side-menu.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+	init() {
+		this._super(...arguments);
+		this.set('menuVisibility', false)
+	},
+
+	toggleMenuVisibility(){
+		this.toggleProperty('menuVisibility');
+	}
 });
