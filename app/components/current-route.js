1c9046343c500a240b926b25f84df827f78025e5
AEROdom <dom.brause@gmail.com>
2016-09-06 22:09:59 +0100

    current route
    + current route returns route on init(); need to check re-render
    + account profile centered
    + better naming

--- app/components/current-route.js
+++ app/components/current-route.js
@@ -8,8 +8,32 @@ init() {
   this.set('appCtrl', appCtrl);
 },
 
-translatedRoute() {
-	
-}
+translatedRoute: Ember.computed('this.appCtrl', function() {
+	let currentRoute = this.appCtrl.currentRouteName;
+	Ember.Logger.info('Current route: ', currentRoute)
+
+	switch(currentRoute) {
+		case ('account.index' || 'account.settings'):
+			return 'Account';
+			break;
+		case ('dashboard.index'):
+			return 'Dashboard help';
+			break;
+		case ('dashboard.board.index'):
+			return 'Dashboard';
+			break;
+		case ('settings'):
+			return 'App settings';
+			break;
+		case ('about'):
+			return 'About <i>this</i>';
+			break;
+
+		default:
+			return 'Oooops!';
+			break;
+			}
+
+		})
 
 });
