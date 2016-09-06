1e207b7b8882236e21219b1e8abaafdb9c0dce30
AEROdom <dom.brause@gmail.com>
2016-09-06 23:21:40 +0100

    JSLint
    ember-dashboard version bump

--- app/components/current-route.js
+++ app/components/current-route.js
@@ -10,31 +10,25 @@ init() {
 
 translatedRoute: Ember.computed('this.appCtrl', function() {
 	let currentRoute = this.appCtrl.currentRouteName;
-	Ember.Logger.info('Current route: ', currentRoute)
+	Ember.Logger.info('Current route: ', currentRoute);
 
 	switch(currentRoute) {
 		case 'account.index':
 		case 'account.settings':
 			return 'Account';
-			break;
 		case 'dashboard.index':
 			return 'Dashboard help';
-			break;
 		case 'dashboard.board.index':
 			return 'Dashboard';
-			break;
 		case 'settings':
 			return 'App settings';
-			break;
 		case 'about':
 			return 'About (this)';
-			break;
 
 		default:
 			return 'Oooops!';
-			break;
 			}
 
-		})
+		}).property('appCtrl')
 
 });
