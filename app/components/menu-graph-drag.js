29362fcda8214fd502e8c13642b16f4db3ac122a
Brause, Dominic <Dominic.Brause@emc.com>
2016-09-12 17:09:28 +0100

    minor templating and CSS animations

--- app/components/menu-graph-drag.js
+++ app/components/menu-graph-drag.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
 
 	actions: {
 		cancelChanges() {
-			this.get('model').rollbackAttributes();
+			this.get('store').rollbackAttributes();
 		},
 		saveChanges() {
-			this.get('model').findRecord('dashboard', param.dashboard_id).then(function(data){
+			this.get('store').findRecord('dashboard', param.dashboard_id).then(function(data){
 				this.set('title', model.name);
 				this.set('text', model.text);
 			});
