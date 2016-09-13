a6961fc22faaeda9c746be2b5b2abb2a8c08427a
Brause, Dominic <Dominic.Brause@emc.com>
2016-09-13 16:51:14 +0100

    + favicons 🎉 + CSS adjustments

--- app/components/menu-graph-drag.js
+++ app/components/menu-graph-drag.js
@@ -2,19 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+	store: Ember.inject.service(),
+
 	actions: {
-		createNewDashboard(){},
+		createNewDashboard(){
+			this.get('store').createRecord('dashboard');
+		},
 
 		rollbackDashboardChanges() {
 			this.get('store').rollbackAttributes();
 		},
-		saveDashboardChanges(id) {
-			this.get('store').findRecord('dashboard', id)
-				.then(function(){
-					this.set('name', this.get('model.name'));
-					this.set('text', this.get('model.text'));
-				});
-			record.save().then(function() {
+		saveDashboardChanges() {
+			// this.get('store').findRecord('dashboard', id)
+			// 	.then(function(){
+			// 		this.set('name', this.get('model.name'));
+			// 		this.set('text', this.get('model.text'));
+			// 	});
+
+			this.get('store').save().then(function() {
 				Ember.Logger.debug('Record saved successfully!');
 			}, function() {
 				Ember.Logger.debug('Saving record failed!');
